695f3b637d5586bb9d35369c8efdfdb3ceb9c393
VadimKurachynski <kurachynski@gmail.com>
2022-12-21 09:48:28 +0300

    45

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ import express from 'express'
 
 const PORT=5000;
 
-const app=express()
+const app=express();
+app.use(express.json());
 
-app.listen(PORT,()=>console.log(`server started on ${PORT}`))
 
 app.get(`/`,(req,res)=>{
+    console.log(req.query);
+    console.log(req.body);
     res.status(200).json("сервер работает")
 })
 
 
-
-console.log("server working")
+app.listen(PORT,()=>console.log(`server started on ${PORT}`))
