55c1c1e8bade90d0673c8e9d1d6722747729cfea
VadimKurachynski <kurachynski@gmail.com>
2022-12-24 17:17:19 +0300

    base

---
+++ db.js
@@ -0,0 +1,11 @@
+const Pool=require('pg').Pool;
+const pool=new Pool({
+    user:"postgres",
+    password:"243388",
+    host:"localhost",
+    port:5432,
+    database: "promnadzor"
+
+});
+module.exports=pool;
+
