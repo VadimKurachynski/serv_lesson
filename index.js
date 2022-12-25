9bac5f488e998ce9b6ee7fe8710a87bd556bca7c
VadimKurachynski <kurachynski@gmail.com>
2022-12-25 23:33:22 +0300

    ftp

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express'
 
-
 const PORT=5000;
 
 const app=express();
 app.use(express.json());
-
 app.get(`/`,(req,res)=>{
     console.log(req.query);
     console.log(req.body);
