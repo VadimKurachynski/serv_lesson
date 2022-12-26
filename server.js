532042ee7956bdc269b42eb5fc5d1605df28053f
VadimKurachynski <kurachynski@gmail.com>
2022-12-26 23:11:26 +0300

    4

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const PORT=3000;
 app.listen(PORT,(error)=>{
 error? console.log(error):console.log(`listening port ${PORT}`);
 });
-
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/1.html");
 });
