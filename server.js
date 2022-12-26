2c2efe0082fc5df2f367c07c06e3a82eb95b203b
VadimKurachynski <kurachynski@gmail.com>
2022-12-26 23:10:54 +0300

    34

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express=require('express');
 const path=require('path');
-
 const app=express();
 const PORT=3000;
-
 app.listen(PORT,(error)=>{
 error? console.log(error):console.log(`listening port ${PORT}`);
 });
